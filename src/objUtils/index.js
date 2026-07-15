50b6752829e30db879db80ec87783d191eeaa235
Omar Gamal <omrgamo1@gmail.com>
2026-07-15 23:37:19 +0300

    add objUtils/filterByIncludeValueForValues & objUtils/filterByValueForValues and fix/add somethings in arrUtils and objUtils

--- src/objUtils/index.js
+++ src/objUtils/index.js
@@ -1,2 +1,4 @@
 export { invalid } from '../invalid.js';
 export { filterByValueForKeys as fbValfk } from './filterByValueForKeys.js';
+export { filterByValueForValues as fbValfv } from './filterByValueForValues.js';
+export { filterByIncludeValueForValues as fbIncludeValfv } from './filterByIncludeValueForValues.js';
