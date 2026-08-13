5f9e61427f3216a973163e8f1f4f96011ad2952c
O-Mar <omrgamo1@gmail.com>
2026-08-13 17:01:59 +0300

    refactor: Export jsonValidator from shared index

--- src/shared/index.js
+++ src/shared/index.js
@@ -4,3 +4,4 @@ export { isWalkable } from './isWalkable.js';
 export { filterEngineRouter } from './filterEngineRouter.js';
 export * from './baseFilterEngine.js';
 export * as reBook from './regexBook.js';
+export { isValidJSONObjectOrArray, isNotValidJSONObjectOrArray } from './jsonValidator.js';
